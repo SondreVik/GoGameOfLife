1aa1dd4bb1daa773bae41bea059671ba584126df
Sondre Eide Vik <soev@equinor.com>
2024-04-05 15:30:57 +0200

    feat: fixed scaling and neighbor check

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 )
 
 const (
-	globalScreenWidth        = 320
-	globalScreenHeight       = 240
-	tps                      = 30
-	scale                    = 2
-	rgbMax             uint8 = 0xff
-	rgbMin             uint8 = 0
-	randomPercent            = 100
+	globalScreenWidth          = 1920
+	globalScreenHeight         = 1080
+	tps                        = 10
+	scale                      = 4
+	alive              uint8   = 0
+	dead               uint8   = 0xff
+	randomPercent      float32 = 5
 )
 
 type Game struct {
@@ -74,15 +74,18 @@ func newPixelState(liveNeighbors int, originalState bool) bool {
 	if liveNeighbors == 3 {
 		return true
 	}
-	return false
+	if liveNeighbors > 3 {
+		return false
+	}
+	return originalState
 }
 
 func drawPixel(img *image.RGBA, x, y int, active bool) *image.RGBA {
 	for yi := 0; yi < scale; yi++ {
 		for xi := 0; xi < scale; xi++ {
-			color := rgbMin
+			color := dead
 			if active {
-				color = rgbMax
+				color = alive
 			}
 			index := (4 * xi) + yi*img.Stride + y*img.Stride*scale + (x * 4 * scale)
 			if img.Pix[index] == color {
@@ -91,7 +94,7 @@ func drawPixel(img *image.RGBA, x, y int, active bool) *image.RGBA {
 			img.Pix[index] = color
 			img.Pix[index+1] = color
 			img.Pix[index+2] = color
-			img.Pix[index+3] = rgbMax
+			img.Pix[index+3] = 0xff
 		}
 	}
 	return img
@@ -105,26 +108,29 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return globalScreenWidth, globalScreenHeight
 }
 
-func initState(x, y int) (state [][]bool) {
-	for yi := 0; yi < y; yi++ {
+func initState(width, height int) (state [][]bool) {
+	for yi := 0; yi < height; yi++ {
 		line := []bool{}
-		for xi := 0; xi < x; xi++ {
-			line = append(line, randomFlag(x, y))
+		for xi := 0; xi < width; xi++ {
+			line = append(line, randomFlag(width, height))
 		}
 		state = append(state, line)
 	}
 	return
 }
 
-func randomFlag(x, y int) bool {
-	return rand.Int()%(x*y) < ((randomPercent / 100) * x * y)
+func randomFlag(width, height int) bool {
+	percentedValue := ((randomPercent / 100) * float32(width) * float32(height))
+	modulo := rand.Int() % (width * height)
+	result := modulo < int(percentedValue)
+	return result
 }
 
 func main() {
-	ebiten.SetWindowSize(globalScreenWidth*2, globalScreenHeight*2)
+	ebiten.SetWindowSize(globalScreenWidth, globalScreenHeight)
 	ebiten.SetWindowTitle("Hello, World!")
 	ebiten.SetTPS(tps)
-	if err := ebiten.RunGame(&Game{canvas: image.NewRGBA(image.Rect(0, 0, globalScreenWidth, globalScreenHeight)), state: initState(globalScreenWidth/4, globalScreenHeight/4)}); err != nil {
+	if err := ebiten.RunGame(&Game{canvas: image.NewRGBA(image.Rect(0, 0, globalScreenWidth, globalScreenHeight)), state: initState(globalScreenWidth/scale, globalScreenHeight/scale)}); err != nil {
 		log.Fatal(err)
 	}
 }
