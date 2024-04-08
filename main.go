71c7f13de9251c182b98f15156dcb635b82e3d4c
Sondre Eide Vik <soev@equinor.com>
2024-04-08 09:18:23 +0200

    chore: cleanup

--- main.go
+++ main.go
@@ -33,7 +33,10 @@ func main() {
 	ebiten.SetWindowSize(settings.GlobalScreenWidth, settings.GlobalScreenHeight)
 	ebiten.SetWindowTitle("Hello, Conway!")
 	ebiten.SetTPS(settings.Tps)
-	if err := ebiten.RunGame(&Game{canvas: image.NewRGBA(image.Rect(0, 0, settings.GlobalScreenWidth, settings.GlobalScreenHeight)), state: gameLogic.InitGame(settings.GlobalScreenWidth/settings.Scale, settings.GlobalScreenHeight/settings.Scale)}); err != nil {
+	err := ebiten.RunGame(&Game{
+		canvas: image.NewRGBA(image.Rect(0, 0, settings.GlobalScreenWidth, settings.GlobalScreenHeight)),
+		state:  gameLogic.InitGame(settings.GlobalScreenWidth/settings.Scale, settings.GlobalScreenHeight/settings.Scale)})
+	if err != nil {
 		log.Fatal(err)
 	}
 }
