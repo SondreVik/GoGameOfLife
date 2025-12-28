80890e58187b5845bff3a9777febc1ca37186f6d
Sondre Eide Vik <sondre_vik@hotmail.com>
2025-12-28 15:22:01 +0100

    chore: update docker command

--- run-on-pi.sh
+++ run-on-pi.sh
@@ -5,10 +5,12 @@
 # Allow X11 connections from Docker
 xhost +local:docker
 
-# Run the container with X11 display access
+# Run the container with X11 display access as the current user
 docker run --rm \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+  -v $HOME/.Xauthority:/root/.Xauthority:rw \
+  --user $(id -u):$(id -g) \
   --device /dev/dri \
   --name go-game-of-life \
   sondreevik/go-arm-app:latest
